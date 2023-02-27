d72a52176ec26540ed5a6ce4b5f8bc3321d0f7eb
Emre Çolak <emrecolak@Emre.home>
2023-02-27 20:49:38 +0300

    reselect library intalled and implement memozation into category selector

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,18 @@
-export const selectorCategoriesMap = (state) =>
-  state.category.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => state.category;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectorCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
